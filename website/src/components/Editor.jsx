5b93e4af1190040636812946c8006269db5a296a
Huynhthuong2505 <your-email@example.com>
2026-07-08 22:29:20 +0700

    Add Monaco Editor

--- website/src/components/Editor.jsx
+++ website/src/components/Editor.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import Editor from "@monaco-editor/react";
 
 export default function CodeEditor() {
+  const [code, setCode] = useState(`function hello() {
+  console.log("Sandbox CodeX");
+}
+
+hello();
+`);
+
   return (
     <Editor
       height="100%"
-      defaultLanguage="javascript"
+      language="javascript"
       theme="vs-dark"
-      defaultValue={'console.log("Sandbox CodeX");'}
+      value={code}
+      onChange={(value) => setCode(value || "")}
+      options={{
+        fontSize: 15,
+        minimap: { enabled: true },
+        automaticLayout: true,
+        wordWrap: "on",
+      }}
     />
   );
 }
